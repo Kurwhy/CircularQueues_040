4de02144335995bc9b47f4c2d6241c115dbab1e7
Wahyu <firmansyahw503@gmail.com>
2023-06-22 10:58:10 +0700

    menghapus element yang berada diakhir array

--- CircularQueues_040.cpp
+++ CircularQueues_040.cpp
@@ -51,7 +51,13 @@ public:
 			FRONT = -1;
 			REAR = -1;
 		}
-
+		else {
+			// Jika element yang dihapus berada diposisi terakhir array, Kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT = 0;
+			else
+				FRONT = FRONT + 1;
+		}
 	}
 		
 };
