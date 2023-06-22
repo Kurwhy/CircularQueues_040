eae89df1834037e9deedb8576a0c983cffff1f3f
Wahyu <firmansyahw503@gmail.com>
2023-06-22 11:21:24 +0700

    tampilan tuk memasukkan data

--- CircularQueues_040.cpp
+++ CircularQueues_040.cpp
@@ -104,6 +104,10 @@ int main() {
 	char ch;
 
 	while (true) {
-
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" <<
+		}
 	}
 }
