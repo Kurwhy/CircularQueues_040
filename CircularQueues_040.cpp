72ce65ab9592265915a4c02716a2c2ce63a373b9
Wahyu <firmansyahw503@gmail.com>
2023-06-22 10:47:58 +0700

    mengecek apakah antrian penuh

--- CircularQueues_040.cpp
+++ CircularQueues_040.cpp
@@ -11,5 +11,16 @@ public:
 		REAR = -1;
 	}
 
-	void 
+	void insert() {
+		int num;
+		cout << "Enter a number ; ";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh.
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overflow\n";
+			return;
+		}
+	}
 };
